2a8046b96c970c22214d3d7cdde87e4f671daafc
sarvdavoudi <sarvdavoudi@gmai.com>
2024-11-25 11:41:19 +0330

    back to a base files

--- test_connection.py
+++ test_connection.py
@@ -1,11 +1,8 @@
 import os
-from dotenv import load_dotenv
 import requests
 
-# Load environment variables from .env file
-load_dotenv()
-
 def test_api_connection():
-    url = os.getenv("API_URL")
+    url = os.getenv("API_URL")    
+    assert url, "API_URL.is not loaded from environment variables"    
     response = requests.get(url)
     assert response.status_code == 200, f"Failed to connect to API. Status code: {response.status_code}"
