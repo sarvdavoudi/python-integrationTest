e71d420a3252db4448ea044037c86e0696000be6
sarv_davoudi <sarvdavoudi@gmail.com>
2024-12-16 10:38:00 +0330

    just for test connection

--- tests/test_admin_login.py
+++ tests/test_admin_login.py
@@ -21,3 +21,4 @@ def test_admin_login(api_url, captcha_key, captcha_response):
     response = requests.post(f"{api_url}/user/login/", json=payload)
     assert response.status_code == 200, "Login failed"
     print('Login Response:', response.json())
+# just for test
