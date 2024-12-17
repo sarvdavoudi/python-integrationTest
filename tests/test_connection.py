58c867b28e5d4b03f66f44d50dee03a74c684e1c
sarv_davoudi <sarvdavoudi@gmail.com>
2024-12-17 15:56:20 +0330

    connection test updated

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,11 +1,11 @@
 import os
 import requests
 
+
 def test_api_connection():
-    url = os.getenv("API_URL")    
-    assert url, "API_URL.is not loaded from environment variables"    
+    url = os.getenv("API_URL")
+    assert url, "API_URL.is not loaded from environment variables"
     response = requests.get(url)
-    assert response.status_code == 200, f"Failed to connect to API. Status code: {response.status_code}"
-    response_data=response.json()
+    response_data = response.json()
     assert response_data["code"] != 1000, f"Test failed:internal server error"
     print("Test passed successfully!")
